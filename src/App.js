441ddcc992f40e5b442f8ad3c069f93c74ec816a
Alison Reed <alison.michelle.reed@gmail.com>
2018-09-19 16:08:17 -0700

    Replace placeholder text in App.js with DayView component (current default view

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './styles/App.css';
 
+import DayView from './components/DayView';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Are you free is coming soon!</h1>
+        <DayView />
       </div>
     );
   }
