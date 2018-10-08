9e1b462dc1e9b458182606ebc4f4b0bd63ff19a1
Alison Reed <alison.michelle.reed@gmail.com>
2018-10-08 17:21:17 -0400

    Replace lists with divs inside nav

--- src/components/CalendarMenu.js
+++ src/components/CalendarMenu.js
@@ -4,12 +4,12 @@ import styles from '../styles/CalendarMenu.css';
 export default class CalendarMenu extends Component {
 	render() {
 		return <nav>
-			<ul>
-				<li><a>Year View</a></li>
-				<li><a>Month View</a></li>
-				<li><a>Week View</a></li>
-				<li><a>Day View</a></li>
-			</ul>
+
+				<div>Year View</div>
+				<div>Month View</div>
+				<div>Week View</div>
+				<div>Day View</div>
+
 		</nav>
 	}
 }
