d27a7843f3f14a1d4ff8c0e4a799d9536b623478
Alison Reed <alison.michelle.reed@gmail.com>
2018-10-08 17:12:25 -0400

    Add semantic nav to menu

--- src/components/CalendarMenu.js
+++ src/components/CalendarMenu.js
@@ -3,11 +3,13 @@ import styles from '../styles/CalendarMenu.css';
 
 export default class CalendarMenu extends Component {
 	render() {
-		return <ul>
-			<li><a>Year View</a></li>
-			<li><a>Month View</a></li>
-			<li><a>Week View</a></li>
-			<li><a>Day View</a></li>
-		</ul>
+		return <nav>
+			<ul>
+				<li><a>Year View</a></li>
+				<li><a>Month View</a></li>
+				<li><a>Week View</a></li>
+				<li><a>Day View</a></li>
+			</ul>
+		</nav>
 	}
 }
