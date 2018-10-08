4f86ab6aac95c7b9f070e96b5bedc53cd88be966
Alison Reed <alison.michelle.reed@gmail.com>
2018-10-08 16:51:55 -0400

    Remove 'set all to free' and 'set all to busy' buttons for now

--- src/components/DayView.js
+++ src/components/DayView.js
@@ -4,26 +4,17 @@ import styles from '../styles/DayView.css';
 import TimeBlocks from './TimeBlocks';
 
 export default class DayView extends Component {
-  state = {
-    allFree: false,
-    allBusy: false
-  }
-
   render() {
-    const {allFree, allBusy} = this.state;
-
     return  <React.Fragment>
       <h2>Daily Calendar</h2>
-      <button onClick={() => {
-        this.setState({allFree: true, allBusy: false});
-      }}>Set all to 'Free'</button>
-      <button onClick={() => {
-        this.setState({allBusy: true, allFree: false})
-      }}>Set all to 'Busy'</button>
       <div id="day-calendar">
 
-        <TimeBlocks abbreviation="AM" allFree={allFree} allBusy={allBusy}/>
-        <TimeBlocks abbreviation="PM" allFree={allFree} allBusy={allBusy}/>
+        <TimeBlocks 
+          abbreviation="AM" 
+        />
+        <TimeBlocks 
+          abbreviation="PM" 
+          />
       </div>
     </React.Fragment>
   }
--- src/components/TimeBlock.js
+++ src/components/TimeBlock.js
@@ -3,23 +3,13 @@ import style from '../styles/TimeBlock.css';
 
 export default class TimeBlock extends React.Component {
   state = {
-    isFree: undefined,
+    isFree: false
   };
 
   render() {
-    const {abbreviation, number, allFree, allBusy} = this.props;
+    const {abbreviation, number} = this.props;
     const {isFree} = this.state;
-    console.log(isFree);
-    let status;
-    if (isFree === undefined) {
-      if (allFree) {
-        status = 'free';
-      } else {
-        status = 'busy';
-      }
-    }else {
-      status = isFree ? 'free' : 'busy';
-    }
+    const status = isFree ? 'free' : 'busy';
 
     return (
       <div className={"timeblock " + status} onClick={() => {
