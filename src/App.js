1ef2a5d82a8542da763b9dd97762821a061e4954
Alison Reed <alison.michelle.reed@gmail.com>
2018-10-08 17:06:04 -0400

    Add Calendar Menu to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './styles/App.css';
 
+import CalendarMenu from './components/CalendarMenu';
 import DayView from './components/DayView';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      	<CalendarMenu />
         <DayView />
       </div>
     );
