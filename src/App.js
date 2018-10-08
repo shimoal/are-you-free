996008f6515a53529d5a8a49864399e34e487209
Alison Reed <alison.michelle.reed@gmail.com>
2018-10-08 17:41:11 -0400

    Add calendar views to App

--- src/App.js
+++ src/App.js
@@ -3,13 +3,35 @@ import './styles/App.css';
 
 import CalendarMenu from './components/CalendarMenu';
 import DayView from './components/DayView';
+import MonthView from './components/MonthView';
+import WeekView from './components/WeekView';
+import YearView from './components/YearView';
 
 class App extends Component {
+	state = {
+		selectedView: 'year'
+	}
+
+	getView() {
+		switch (this.state.selectedView) {
+			case 'day':
+				return <DayView />;
+			case 'week':
+				return <WeekView />;
+			case 'month':
+				return <MonthView />;
+			case 'year':
+				return <YearView />;
+			default: 
+				return <div />
+		}
+	}
+
   render() {
     return (
       <div className="App">
       	<CalendarMenu />
-        <DayView />
+        {this.getView()}
       </div>
     );
   }
