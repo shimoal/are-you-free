f9aabb3d1a9196cda3515450d7226b7fabd6ca8b
Alison Reed <alison.michelle.reed@gmail.com>
2018-10-08 17:34:46 -0400

    Create components for Month, Week, and Day views

---
+++ src/components/MonthView.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class MonthView extends Component {
+	render() {
+		return <div />
+	}
+}
---
+++ src/components/WeekView.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class WeekView extends Component {
+	render() {
+		return <div />
+	}
+}
