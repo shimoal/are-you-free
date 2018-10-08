3894110013256e2e9440f5435924c91847330690
Alison Reed <alison.michelle.reed@gmail.com>
2018-10-08 17:11:45 -0400

    Add styles to make menu horizontal

--- src/components/CalendarMenu.js
+++ src/components/CalendarMenu.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import styles from '../styles/CalendarMenu.css';
 
 export default class CalendarMenu extends Component {
 	render() {
 		return <ul>
-			<li>Year View</li>
-			<li>Month View</li>
-			<li>Week View</li>
-			<li>Day View</li>
+			<li><a>Year View</a></li>
+			<li><a>Month View</a></li>
+			<li><a>Week View</a></li>
+			<li><a>Day View</a></li>
 		</ul>
 	}
 }
