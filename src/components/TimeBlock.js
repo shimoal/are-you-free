e69ce986c55e72c10ce31ac024c2f4f8428c0843
Alison Reed <alison.michelle.reed@gmail.com>
2018-10-08 16:27:55 -0400

    Progress on time block bug

--- src/components/TimeBlock.js
+++ src/components/TimeBlock.js
@@ -3,18 +3,21 @@ import style from '../styles/TimeBlock.css';
 
 export default class TimeBlock extends React.Component {
   state = {
-    isFree: false,
+    isFree: undefined,
   };
 
   render() {
     const {abbreviation, number, allFree, allBusy} = this.props;
     const {isFree} = this.state;
+    console.log(isFree);
     let status;
-    if (allFree) {
-      status = 'free';
-    } else if (allBusy) {
-      status = 'busy';
-    } else {
+    if (isFree === undefined) {
+      if (allFree) {
+        status = 'free';
+      } else {
+        status = 'busy';
+      }
+    }else {
       status = isFree ? 'free' : 'busy';
     }
 
--- src/components/TimeBlocks.js
+++ src/components/TimeBlocks.js
@@ -6,12 +6,14 @@ import TimeBlock from './TimeBlock';
 export default function(props) {
   const timeblocks = [];
   const {allFree, allBusy} = props;
+  console.log('all Free in timeblocks', allFree);
   for (let i = 0; i < 12; i++) {
     timeblocks.push(<TimeBlock 
       abbreviation={props.abbreviation}
       number={i ? i : 12}
       allFree={allFree}
       allBusy={allBusy}
+      isFree={allFree}
       />);
   }
 
