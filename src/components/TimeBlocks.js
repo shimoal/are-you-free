a8a4f18f3d3aa064c70c77d4a5eac2daa55dc8c7
Alison Reed <alison.michelle.reed@gmail.com>
2018-10-09 16:09:23 -0400

    Replace TimeBlock with TimeUnit and delete TimeBlock

--- src/components/TimeBlocks.js
+++ src/components/TimeBlocks.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import '../styles/TimeBlocks.css';
 
-import TimeBlock from './TimeBlock';
+import TimeUnit from './TimeUnit';
 
 export default function(props) {
   const timeblocks = [];
 
   for (let i = 0; i < 12; i++) {
-    timeblocks.push(<TimeBlock 
-      abbreviation={props.abbreviation}
-      key={i}
-      number={i ? i : 12}
-      />);
+    const number= i ? i : 12;
+    const label = number + ':00' + props.abbreviation;
+
+    timeblocks.push(
+      <TimeUnit label={label} height={100} width={180} />
+    );
   }
 
   return (
