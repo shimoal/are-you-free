96552db874d516d5cc27962a82b297571ae31e4a
Alison Reed <alison.michelle.reed@gmail.com>
2018-10-09 14:47:45 -0400

    Add styling for menu

--- src/components/CalendarMenuItem.js
+++ src/components/CalendarMenuItem.js
@@ -3,10 +3,13 @@ import React from 'react';
 
 export default function(props) {
 	const  { selectedView, setView, view } = props;
+	
 	const viewName = view.charAt(0).toUpperCase() + view.slice(1);
+	const className = selectedView === view ? 'selected' : 'default';
 
 	return <div 
-				class={selectedView === view ? 'selected' : 'default'} 
-				onClick={() => setView(view)}>{viewName} View
+				className={className}
+				onClick={() => setView(view)}>
+					{viewName} View
 	</div>;
 }
