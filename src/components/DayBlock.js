13957bbdb76b1eb5e257d9033bc6c2a17b3bc3eb
Alison Reed <alison.michelle.reed@gmail.com>
2018-10-09 15:45:04 -0400

    Move day labels to inside day block

--- src/components/DayBlock.js
+++ src/components/DayBlock.js
@@ -3,8 +3,7 @@ import React from 'react';
 import '../styles/DayBlock.css';
 
 export default function(props) {
-	return <div className="dayBlock">
-		<h3>{props.name}</h3>
-		<div className="day" />
+	return <div className="day">
+			{props.name}
 	</div>;
 }
