c28d767e9efe0245b0e6a9dff077a2af21f1fdd1
Alison Reed <alison.michelle.reed@gmail.com>
2018-10-09 15:53:37 -0400

    Replace DayBlock with new TimeUnit Component and delete DayBlock

--- src/components/DayBlock.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import '../styles/DayBlock.css';
-
-export default function(props) {
-	return <div className="day">
-			{props.name}
-	</div>;
-}
--- src/components/WeekView.js
+++ src/components/WeekView.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import '../styles/DayBlock.css';
 
-import DayBlock from './DayBlock';
+import TimeUnit from './TimeUnit';
 
 const DAY_NAMES = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
@@ -11,7 +11,7 @@ export default class WeekView extends Component {
       <h2>Weekly Calendar</h2>
       <div className="weekView">
 	      {DAY_NAMES.map((name, i) => {
-	      	return <DayBlock name={name} key={i} />
+	      	return <TimeUnit label={name} key={i} height={400} width={120} />
       })}
       </div>
     </React.Fragment>
