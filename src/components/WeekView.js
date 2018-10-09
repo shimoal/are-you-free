3ee5de54fe164582ef37f690e14ffde4e525f293
Alison Reed <alison.michelle.reed@gmail.com>
2018-10-09 14:49:48 -0400

    Create DayBlock component

--- src/components/WeekView.js
+++ src/components/WeekView.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
+import DayBlock from './DayBlock';
+
 export default class WeekView extends Component {
 	render() {
 		return <React.Fragment>
       <h2>Weekly Calendar</h2>
+      <DayBlock />
     </React.Fragment>
 	}
 }
