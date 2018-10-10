840b66a275634b21d5c45ceb46caeb543b762036
Alison Reed <alison.michelle.reed@gmail.com>
2018-10-10 16:52:57 -0400

    Remove unneeded helper functions from MonhView

--- src/components/MonthView.js
+++ src/components/MonthView.js
@@ -5,26 +5,19 @@ import TimeUnit from "./TimeUnit";
 import { MONTH_NAMES, MONTH_NUMBER_OF_DAYS } from "../helpers/constants";
 
 export default class MonthView extends Component {
-	getMonth() {
-		const { date } = this.props;
-		const monthIndex = date.getMonth();
-		return MONTH_NAMES[monthIndex];
-	}
-
-	getNumberOfDaysInMonth() {
-		const month = this.getMonth();
-		const daysInMonth = MONTH_NUMBER_OF_DAYS[month];
-		return daysInMonth;
-	}
-
 	render() {
+		const { date } = this.props;
+
+		const monthIndex = date.getMonth();
+		const month = MONTH_NAMES[monthIndex];
+		const daysInMonth = MONTH_NUMBER_OF_DAYS[month];
+
 		const days = [];
-		const daysInMonth = this.getNumberOfDaysInMonth();
-		const month = this.getMonth();
 
 		for (let i = 1; i <= daysInMonth; i++) {
 			days.push(<TimeUnit label={i} key={i} height={80} width={60} />);
 		}
+
 		return (
 			<div className="calendar">
 				<h2>{month}</h2>
