857ca4b5beb5cedf053e8f8ef4f890b88caf8d51
Alison Reed <alison.michelle.reed@gmail.com>
2018-10-10 16:19:00 -0400

    Remove helper functions from DayView, refactor to use fewer lines

--- src/components/DayView.js
+++ src/components/DayView.js
@@ -5,25 +5,16 @@ import TimeBlocks from "./TimeBlocks";
 import { DAY_NAMES, MONTH_NAMES } from "../helpers/constants";
 
 export default class DayView extends Component {
-  getDayName() {
-    const dayIndex = this.props.date.getDay();
-    return DAY_NAMES[dayIndex];
-  }
-
-  getMonthName() {
-    const monthIndex = this.props.date.getMonth();
-    return MONTH_NAMES[monthIndex];
-  }
-
   render() {
-    const day = this.getDayName();
-    const month = this.getMonthName();
-    const date = this.props.date.getDate();
+    const { date } = this.props;
+
+    const day = DAY_NAMES[date.getDay()];
+    const month = MONTH_NAMES[date.getMonth()];
 
     return (
       <React.Fragment>
         <h2>
-          {day}, {month} {date}
+          {day}, {month} {date.getDate()}
         </h2>
         <div id="day-calendar">
           <TimeBlocks abbreviation="AM" />
