a528768f00afeae4b47295e1b2657e623d8b9c1d
Alison Reed <alison.michelle.reed@gmail.com>
2018-10-09 21:53:20 -0400

    Delete day block css

--- src/components/YearView.js
+++ src/components/YearView.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 
+import TimeUnit from './TimeUnit';
+
+const MONTH_NAMES = [
+	'January',
+	'February',
+	'March',
+	'April',
+	'May',
+	'June',
+	'July',
+	'August',
+	'September',
+	'October',
+	'November',
+	'December'
+];
+
 export default class YearView extends Component {
 	render() {
 		return <React.Fragment>
       <h2>Yearly Calendar</h2>
+      <div className="yearView">
+      	{MONTH_NAMES.map((name, i) => {
+      		return <TimeUnit label={name} key={i} height={200} width={200} />
+      	})} 
+      </div>
     </React.Fragment>
 	}
 }
