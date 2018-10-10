1c6f186e598f6d4a4624ef97a670a493c607ab6e
Alison Reed <alison.michelle.reed@gmail.com>
2018-10-10 15:16:20 -0400

    Add styling to week view to align week days into a single row

--- src/components/WeekView.js
+++ src/components/WeekView.js
@@ -1,18 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "../styles/WeekView.css";
 
-import TimeUnit from './TimeUnit';
+import TimeUnit from "./TimeUnit";
 
-const DAY_NAMES = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const DAY_NAMES = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
 
 export default class WeekView extends Component {
 	render() {
-		return <React.Fragment>
-      <h2>Weekly Calendar</h2>
-      <div className="weekView">
-	      {DAY_NAMES.map((name, i) => {
-	      	return <TimeUnit label={name} key={i} height={400} width={120} />
-      })}
-      </div>
-    </React.Fragment>
+		return (
+			<div className="calendar">
+				<h2>Weekly Calendar</h2>
+				<div id="weekView">
+					{DAY_NAMES.map((name, i) => {
+						return <TimeUnit label={name} key={i} height={300} width={60} />;
+					})}
+				</div>
+			</div>
+		);
 	}
 }
