ffa153bd771dce147df1b62a8df63a84979fb323
Alison Reed <alison.michelle.reed@gmail.com>
2018-10-10 16:16:04 -0400

    Display month and date along with day of the week on day view

--- src/components/DayView.js
+++ src/components/DayView.js
@@ -5,17 +5,26 @@ import TimeBlocks from "./TimeBlocks";
 import { DAY_NAMES, MONTH_NAMES } from "../helpers/constants";
 
 export default class DayView extends Component {
-  getDayOfWeek() {
+  getDayName() {
     const dayIndex = this.props.date.getDay();
     return DAY_NAMES[dayIndex];
   }
 
+  getMonthName() {
+    const monthIndex = this.props.date.getMonth();
+    return MONTH_NAMES[monthIndex];
+  }
+
   render() {
-    const day = this.getDayOfWeek();
+    const day = this.getDayName();
+    const month = this.getMonthName();
+    const date = this.props.date.getDate();
 
     return (
       <React.Fragment>
-        <h2>{day}</h2>
+        <h2>
+          {day}, {month} {date}
+        </h2>
         <div id="day-calendar">
           <TimeBlocks abbreviation="AM" />
           <TimeBlocks abbreviation="PM" />
