7f1e333765740e49b2795842e6852a44fa95e13a
Alison Reed <alison.michelle.reed@gmail.com>
2018-10-10 14:59:05 -0400

    Add styling for year view

--- src/components/YearView.js
+++ src/components/YearView.js
@@ -1,31 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "../styles/YearView.css";
 
-import TimeUnit from './TimeUnit';
+import TimeUnit from "./TimeUnit";
 
 const MONTH_NAMES = [
-	'January',
-	'February',
-	'March',
-	'April',
-	'May',
-	'June',
-	'July',
-	'August',
-	'September',
-	'October',
-	'November',
-	'December'
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
 ];
 
 export default class YearView extends Component {
 	render() {
-		return <React.Fragment>
-      <h2>Yearly Calendar</h2>
-      <div className="yearView">
-      	{MONTH_NAMES.map((name, i) => {
-      		return <TimeUnit label={name} key={i} height={200} width={200} />
-      	})} 
-      </div>
-    </React.Fragment>
+		return (
+			<React.Fragment>
+				<h2>Yearly Calendar</h2>
+				<div id="yearView">
+					{MONTH_NAMES.map((name, i) => {
+						return <TimeUnit label={name} key={i} height={200} width={200} />;
+					})}
+				</div>
+			</React.Fragment>
+		);
 	}
 }
