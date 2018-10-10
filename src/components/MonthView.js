914848e6035696fc84b3837471e1db9b7804171c
Alison Reed <alison.michelle.reed@gmail.com>
2018-10-10 16:47:27 -0400

    Display name of month as title of month view

--- src/components/MonthView.js
+++ src/components/MonthView.js
@@ -5,10 +5,14 @@ import TimeUnit from "./TimeUnit";
 import { MONTH_NAMES, MONTH_NUMBER_OF_DAYS } from "../helpers/constants";
 
 export default class MonthView extends Component {
-	getNumberOfDaysInMonth() {
+	getMonth() {
 		const { date } = this.props;
 		const monthIndex = date.getMonth();
-		const month = MONTH_NAMES[monthIndex];
+		return MONTH_NAMES[monthIndex];
+	}
+
+	getNumberOfDaysInMonth() {
+		const month = this.getMonth();
 		const daysInMonth = MONTH_NUMBER_OF_DAYS[month];
 		return daysInMonth;
 	}
@@ -16,13 +20,14 @@ export default class MonthView extends Component {
 	render() {
 		const days = [];
 		const daysInMonth = this.getNumberOfDaysInMonth();
+		const month = this.getMonth();
 
 		for (let i = 1; i <= daysInMonth; i++) {
 			days.push(<TimeUnit label={i} key={i} height={80} width={60} />);
 		}
 		return (
 			<div className="calendar">
-				<h2>Monthly Calendar</h2>
+				<h2>{month}</h2>
 				<div id="month-calendar">{days}</div>
 			</div>
 		);
