f013424a0d51608082c0fd10a608caa04ac3f357
Alison Reed <alison.michelle.reed@gmail.com>
2018-10-10 16:07:30 -0400

    Create constants file for reusable constant variables

---
+++ src/helpers/constants.js
@@ -0,0 +1,26 @@
+const DAYS_OF_WEEK = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
+
+const MONTH_NAMES = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+
+export { DAYS_OF_WEEK, MONTH_NAMES };
