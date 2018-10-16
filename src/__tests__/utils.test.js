59ebf1eb1f60b51daffa2e9dde41714652433c4b
Alison Reed <alison.michelle.reed@gmail.com>
2018-10-16 14:33:22 -0400

    Add tests for getNumDaysInMonth

---
+++ src/__tests__/utils.test.js
@@ -0,0 +1,34 @@
+import utils from "./../helpers/utils";
+
+const { getNumDaysInMonth } = utils;
+
+describe("getNumDaysInMonth", () => {
+	it("should exist", () => {
+		expect(getNumDaysInMonth).toBeDefined();
+	});
+
+	it("should return the correct number of days for every month", () => {
+		expect(getNumDaysInMonth(0, 2018)).toEqual(31); //January has 31 days
+		expect(getNumDaysInMonth(1, 2018)).toEqual(28); //February has 28 days (on non-leap years)
+		expect(getNumDaysInMonth(2, 2018)).toEqual(31); //March has 31 days
+		expect(getNumDaysInMonth(3, 2018)).toEqual(30); //April has 30 days
+		expect(getNumDaysInMonth(4, 2018)).toEqual(31); //May has 31 days
+		expect(getNumDaysInMonth(5, 2018)).toEqual(30); //June has 30 days
+		expect(getNumDaysInMonth(6, 2018)).toEqual(31); //July has 31 days
+		expect(getNumDaysInMonth(7, 2018)).toEqual(31); //August has 31 days
+		expect(getNumDaysInMonth(8, 2018)).toEqual(30); //September has 30 days
+		expect(getNumDaysInMonth(9, 2018)).toEqual(31); //October has 31 days
+		expect(getNumDaysInMonth(10, 2018)).toEqual(30); //November has 30 days
+		expect(getNumDaysInMonth(11, 2018)).toEqual(31); //December has 31 days
+	});
+
+	it("should return the correct number of days for February, including leap years", () => {
+		expect(getNumDaysInMonth(1, 2016)).toEqual(29); //2016 is a leap year
+		expect(getNumDaysInMonth(1, 2020)).toEqual(29); //2020 is a leap year
+		expect(getNumDaysInMonth(1, 2000)).toEqual(29); //2000 is  a leap year
+		expect(getNumDaysInMonth(1, 1900)).toEqual(28); //1900 is not a leap year
+		expect(getNumDaysInMonth(1, 2011)).toEqual(28); //2011 is not a leap year
+		expect(getNumDaysInMonth(1, 1683)).toEqual(28); //1683 is not a leap year
+		expect(getNumDaysInMonth(1, 1804)).toEqual(29); //1804 is  a leap year
+	});
+});
