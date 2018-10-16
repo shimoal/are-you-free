fa453d99285579894005ba7870e339041098d6c8
Alison Reed <alison.michelle.reed@gmail.com>
2018-10-16 13:55:49 -0400

    Add helper functions to calculate dates for Sunday start date and Saturday end date for each week

--- src/components/WeekView.js
+++ src/components/WeekView.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "../styles/WeekView.css";
 
 import TimeUnit from "./TimeUnit";
-import { DAY_NAMES } from "../helpers/constants";
+import { DAY_NAMES, MONTH_NAMES } from "../helpers/constants";
 import { getNumDaysInMonth } from "../helpers/utils";
 
 export default class WeekView extends Component {
@@ -10,16 +10,64 @@ export default class WeekView extends Component {
 		const { date } = this.props;
 		const currentdate = date.getDate();
 		const dayOfWeek = date.getDay();
-		const sundayDate = currentdate - dayOfWeek;
-		const saturdayDate = 6 - dayOfWeek + currentdate;
+		const sundayDate = this.getSundayDate();
+		const saturdayDate = this.getSaturdayDate();
 		return `${sundayDate} - ${saturdayDate}`;
 	}
 
-	getSundayMonthIndex(sundayDate, currentMonthIndex) {
+	getPreviousMonth(currentMonth) {
+		return currentMonth === 0 ? 11 : currentMonth - 1;
+	}
+
+	getNextMonth(currentMonth) {
+		return currentMonth === 11 ? 0 : currentMonth + 1;
+	}
+
+	getDaysInPreviousMonth(currentMonth, currentYear) {
+		const prevMonthIndex = this.getPreviousMonth(currentMonth);
+
+		const prevMonthYear = currentMonth === 0 ? currentYear - 1 : currentYear;
+
+		return getNumDaysInMonth(prevMonthIndex, prevMonthYear);
+	}
+
+	getSundayDate() {
+		const { date } = this.props;
+		const monthIndex = date.getMonth();
+		const year = date.getFullYear();
+
+		const sundayDate = date.getDate() - date.getDay();
+
 		if (sundayDate > 0) {
-			return currentMonthIndex;
+			const currentMonth = MONTH_NAMES[monthIndex];
+			return `${currentMonth} ${sundayDate}`;
 		}
-		return currentMonthIndex === 0 ? 11 : currentMonthIndex - 1;
+
+		const prevMonthIndex = this.getPreviousMonth(monthIndex);
+		const daysInPrevMonth = this.getDaysInPreviousMonth(monthIndex, year);
+
+		const prevMonth = MONTH_NAMES[prevMonthIndex];
+		const dateDay = daysInPrevMonth + sundayDate;
+
+		return `${prevMonth} ${dateDay}`;
+	}
+
+	getSaturdayDate() {
+		const { date } = this.props;
+		const currentdate = date.getDate();
+		const dayOfWeek = date.getDay();
+		const saturdayDate = 6 - dayOfWeek + currentdate;
+		const daysInCurrentMonth = getNumDaysInMonth(
+			date.getMonth(),
+			date.getFullYear()
+		);
+		if (saturdayDate <= daysInCurrentMonth) {
+			return `${MONTH_NAMES[date.getMonth()]} ${saturdayDate}`;
+		}
+		const day = saturdayDate - daysInCurrentMonth;
+		const nextMonthIndex = this.getNextMonth(date.getMonth());
+
+		return `${MONTH_NAMES[nextMonthIndex]} ${day}`;
 	}
 
 	render() {
