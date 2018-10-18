aa4cf1f589f80d73459861dce346765cabd197e1
Alison Reed <alison.michelle.reed@gmail.com>
2018-10-18 13:40:34 -0400

    Add tests for getPreviousMonth function

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,6 +1,11 @@
 import utils from "./../helpers/utils";
 
-const { getNextMonth, getNumDaysInMonth } = utils;
+const {
+	getNextMonth,
+	getNumDaysInMonth,
+	getNumDaysInPreviousMonth,
+	getPreviousMonth
+} = utils;
 
 describe("getNextMonth", () => {
 	it("should exist", () => {
@@ -51,5 +56,57 @@ describe("getNumDaysInMonth", () => {
 		expect(getNumDaysInMonth(1, 2011)).toEqual(28); //2011 is not a leap year
 		expect(getNumDaysInMonth(1, 1683)).toEqual(28); //1683 is not a leap year
 		expect(getNumDaysInMonth(1, 1804)).toEqual(29); //1804 is  a leap year
+	});
+});
+
+describe("getNumDaysInPreviousMonth", () => {
+	it("should be defined", () => {
+		expect(getNumDaysInPreviousMonth).toBeDefined();
+	});
+
+	it("should return the correct number of days of the previous month for every month in a certain year", () => {
+		expect(getNumDaysInPreviousMonth(0, 2018)).toEqual(31); //December has 31 days
+		expect(getNumDaysInPreviousMonth(1, 2018)).toEqual(31); //January has 31 days
+		expect(getNumDaysInPreviousMonth(2, 2018)).toEqual(28); //February has 28 days (on non-leap years)
+		expect(getNumDaysInPreviousMonth(3, 2018)).toEqual(31); //March has 31 days
+		expect(getNumDaysInPreviousMonth(4, 2018)).toEqual(30); //April has 30 days
+		expect(getNumDaysInPreviousMonth(5, 2018)).toEqual(31); //May has 31 days
+		expect(getNumDaysInPreviousMonth(6, 2018)).toEqual(30); //June has 30 days
+		expect(getNumDaysInPreviousMonth(7, 2018)).toEqual(31); //July has 31 days
+		expect(getNumDaysInPreviousMonth(8, 2018)).toEqual(31); //August has 31 days
+		expect(getNumDaysInPreviousMonth(9, 2018)).toEqual(30); //Septemer has 30 days
+		expect(getNumDaysInPreviousMonth(10, 2018)).toEqual(31); //October has 31 days
+		expect(getNumDaysInPreviousMonth(11, 2018)).toEqual(30); //November has 30 days
+	});
+
+	it("should return the correct number of days for February (when current month is March), including leap years", () => {
+		expect(getNumDaysInPreviousMonth(2, 2016)).toEqual(29); //2016 is a leap year
+		expect(getNumDaysInPreviousMonth(2, 2020)).toEqual(29); //2020 is a leap year
+		expect(getNumDaysInPreviousMonth(2, 2000)).toEqual(29); //2000 is  a leap year
+		expect(getNumDaysInPreviousMonth(2, 1900)).toEqual(28); //1900 is not a leap year
+		expect(getNumDaysInPreviousMonth(2, 2011)).toEqual(28); //2011 is not a leap year
+		expect(getNumDaysInPreviousMonth(2, 1683)).toEqual(28); //1683 is not a leap year
+		expect(getNumDaysInPreviousMonth(2, 1804)).toEqual(29); //1804 is  a leap year
+	});
+});
+
+describe("getPreviousMonth", () => {
+	it("should be defined", () => {
+		expect(getPreviousMonth).toBeDefined();
+	});
+
+	it("should return the index of the previous month", () => {
+		expect(getPreviousMonth(0)).toEqual(11); // Index of January (0) should return index of December (11)
+		expect(getPreviousMonth(1)).toEqual(0); // Index of February (1) should return index of January (0)
+		expect(getPreviousMonth(2)).toEqual(1); // Index of March should (2) return index of February (1)
+		expect(getPreviousMonth(3)).toEqual(2); // Index of April (3) should return index of March (2)
+		expect(getPreviousMonth(4)).toEqual(3); // Index of May (4) should return index of April (3)
+		expect(getPreviousMonth(5)).toEqual(4); // Index of June (5) should return index of May (4)
+		expect(getPreviousMonth(6)).toEqual(5); // Index of July (6) should return index of June (5)
+		expect(getPreviousMonth(7)).toEqual(6); // Index of August (7) should return index of July (6)
+		expect(getPreviousMonth(8)).toEqual(7); // Index of September (8) should return index of August (7)
+		expect(getPreviousMonth(9)).toEqual(8); // Index of October (9) should return index of September (8)
+		expect(getPreviousMonth(10)).toEqual(9); // Index of November (10) should return index of October (9)
+		expect(getPreviousMonth(11)).toEqual(10); // Index of December (11) should return index of November (10)
 	});
 });
