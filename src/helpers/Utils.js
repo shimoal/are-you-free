660f37250717143ae7e6d7e2fc3781c73abd7529
Alison Reed <alison.michelle.reed@gmail.com>
2018-10-18 13:46:54 -0400

    Moved getSaturdayDate and getSundayDate into utils file, changing to getWeekStartingDate and getWeekEndingDate

--- src/helpers/Utils.js
+++ src/helpers/Utils.js
@@ -23,9 +23,52 @@ const getPreviousMonth = currentMonth => {
 	return currentMonth === 0 ? 11 : currentMonth - 1;
 };
 
+// Week ends on Saturday
+// Returns a "MONTH DAY" string of the ending date for the week of the date provided
+getWeekEndingDate = date => {
+	const currentdate = date.getDate();
+	const dayOfWeek = date.getDay();
+	const saturdayDate = 6 - dayOfWeek + currentdate;
+	const daysInCurrentMonth = getNumDaysInMonth(
+		date.getMonth(),
+		date.getFullYear()
+	);
+	if (saturdayDate <= daysInCurrentMonth) {
+		return `${MONTH_NAMES[date.getMonth()]} ${saturdayDate}`;
+	}
+	const day = saturdayDate - daysInCurrentMonth;
+	const nextMonthIndex = getNextMonth(date.getMonth());
+
+	return `${MONTH_NAMES[nextMonthIndex]} ${day}`;
+};
+
+// Week starts on a Sunday
+// Returns a "MONTH DAY" string of the starting date for the week of the date provided
+const getWeekStartingDate = date => {
+	const monthIndex = date.getMonth();
+	const year = date.getFullYear();
+
+	const sundayDate = date.getDate() - date.getDay();
+
+	if (sundayDate > 0) {
+		const currentMonth = MONTH_NAMES[monthIndex];
+		return `${currentMonth} ${sundayDate}`;
+	}
+
+	const prevMonthIndex = getPreviousMonth(monthIndex);
+	const daysInPrevMonth = getNumDaysInPreviousMonth(monthIndex, year);
+
+	const prevMonth = MONTH_NAMES[prevMonthIndex];
+	const dateDay = daysInPrevMonth + sundayDate;
+
+	return `${prevMonth} ${dateDay}`;
+};
+
 module.exports = {
 	getNextMonth,
 	getNumDaysInMonth,
 	getNumDaysInPreviousMonth,
-	getPreviousMonth
+	getPreviousMonth,
+	getWeekEndingDate,
+	getWeekStartingDate
 };
