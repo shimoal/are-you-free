9c20de3df170da4c7db7e0c95eb43aa15d0c744b
Alison Reed <alison.michelle.reed@gmail.com>
2018-10-22 18:01:53 -0400

    Remove code from week view

--- src/components/WeekView.js
+++ src/components/WeekView.js
@@ -3,18 +3,18 @@ import "../styles/WeekView.css";
 
 import TimeUnit from "./TimeUnit";
 import { DAY_NAMES } from "../helpers/constants";
-import { getWeekDates } from "../helpers/utils";
+// import { getWeekDates } from "../helpers/utils";
 
 export default class WeekView extends Component {
 	render() {
 		return (
 			<div className="calendar">
-				<h2>{getWeekDates(this.props.date)}</h2>
-				<div id="weekView">
-					{DAY_NAMES.map((name, i) => {
-						return <TimeUnit label={name} key={i} height={300} width={60} />;
-					})}
-				</div>
+				// <h2>{getWeekDates(this.props.date)}</h2>
+				// <div id="weekView">
+				// 	{DAY_NAMES.map((name, i) => {
+				// 		return <TimeUnit label={name} key={i} height={300} width={60} />;
+				// 	})}
+				// </div>
 			</div>
 		);
 	}
