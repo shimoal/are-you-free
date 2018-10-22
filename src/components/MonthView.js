dc9a7fc3c64f38a6e2d48706bdb4781a14946edb
Alison Reed <alison.michelle.reed@gmail.com>
2018-10-22 17:59:05 -0400

    remove code from monthview causing error in prod

--- src/components/MonthView.js
+++ src/components/MonthView.js
@@ -3,26 +3,28 @@ import "../styles/MonthView.css";
 
 import TimeUnit from "./TimeUnit";
 import { MONTH_NAMES } from "../helpers/constants";
-import { getNumDaysInMonth } from "../helpers/utils";
+// import { getNumDaysInMonth } from "../helpers/utils";
 
 export default class MonthView extends Component {
 	render() {
-		const { date } = this.props;
+		// const { date } = this.props;
 
-		const monthIndex = date.getMonth();
-		const monthName = MONTH_NAMES[monthIndex];
-		const daysInMonth = getNumDaysInMonth(monthIndex, date.getYear());
+		// const monthIndex = date.getMonth();
+		// const monthName = MONTH_NAMES[monthIndex];
+		// const daysInMonth = getNumDaysInMonth(monthIndex, date.getYear());
 
-		const days = [];
+		// const days = [];
 
-		for (let i = 1; i <= daysInMonth; i++) {
-			days.push(<TimeUnit label={i} key={i} height={80} width={60} />);
-		}
+		// for (let i = 1; i <= daysInMonth; i++) {
+		// 	days.push(<TimeUnit label={i} key={i} height={80} width={60} />);
+		// }
 
 		return (
 			<div className="calendar">
-				<h2>{monthName}</h2>
-				<div id="month-calendar">{days}</div>
+				{
+					// <h2>{monthName}</h2>
+					// <div id="month-calendar">{days}</div>
+				}
 			</div>
 		);
 	}
