d7e3641c262bf14ac361c79a68c19409a599959c
Alison Reed <alison.michelle.reed@gmail.com>
2018-10-30 17:54:35 -0400

    Add production build steps for heroku

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 
+if (process.env.NODE_ENV === "production") {
+	// serve production assests
+	app.use(express.static("client/build"));
+	// serve html file if non specified route
+	const path = require("path");
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
