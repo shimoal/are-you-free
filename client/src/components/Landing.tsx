3b63bf3c1b87d6ad40c7a4074f7fbec6f2ef7e90
Alison Reed <alison.michelle.reed@gmail.com>
2018-11-08 23:59:28 -0500

    Change title color back to orange

--- client/src/components/Landing.tsx
+++ client/src/components/Landing.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 export default () => {
 	return (
 		<div>
-			<h1 className="header center teal-text">Are You Free?</h1>
+			<h1 className="header center orange-text">Are You Free?</h1>
 			<div className="row center">
 				<h5 className="header col s12 light">
 					Easy and simple event coordination for groups!
