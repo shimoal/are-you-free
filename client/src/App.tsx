7a5e03dea504c028c1625ff64856e37d470f09f9
Alison Reed <alison.michelle.reed@gmail.com>
2018-11-08 23:58:42 -0500

    Create NavBar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,13 @@ const { Component } = React;
 import "./styles/App.css";
 
 import Landing from "./components/Landing";
+import NavBar from "./components/NavBar";
 
 class App extends Component<{}, { selectedView: string }> {
 	render() {
 		return (
 			<div className="container">
+				<NavBar />
 				<Landing />
 			</div>
 		);
---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+
+export default () => {
+	return (
+		<nav className="light-blue lighten-1">
+			<div className="nav-wrapper container">
+				<a id="logo-container" className="brand-logo" href="#">
+					Logo
+				</a>
+				<ul className="right hide-on-med-and-down">
+					<li>
+						<a href="#">Navbar Link</a>
+					</li>
+				</ul>
+				<ul
+					id="nav-mobile"
+					className="sidenav"
+					style={{ transform: "translateX(-105%)" }}
+				>
+					<li>
+						<a href="#">Navbar Link</a>
+					</li>
+				</ul>
+				<a className="sidenav=trigger" href="#" data-target="nav-mobile">
+					<i className="material-icons">menu</i>
+				</a>
+			</div>
+		</nav>
+	);
+};
