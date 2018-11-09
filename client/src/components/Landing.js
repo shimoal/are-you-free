d1d1761931bf6f10f666159ae17a5d1f1614d02d
Alison Reed <alison.michelle.reed@gmail.com>
2018-11-08 19:12:48 -0500

    Add copy for landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,5 +1,10 @@
 import * as React from "react";
 
 export default () => {
-	return <div>Landing</div>;
+	return (
+		<div style={{ textAlign: "center" }}>
+			<h1>Are You Free</h1>
+			Easy and simple event coordination for groups!
+		</div>
+	);
 };
