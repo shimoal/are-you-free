950694d2f393463c4f43cc41a33870a7ce5b7004
Alison Reed <alison.michelle.reed@gmail.com>
2018-11-09 00:39:19 -0500

    Add React router to route app component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,25 @@
 import * as React from "react";
 const { Component } = React;
+import { BrowserRouter, Route } from "react-router-dom";
 
 import Landing from "./components/Landing";
 import NavBar from "./components/NavBar";
+
+const Dashboard = () => <div>Dashboard</div>;
+const CreateEvent = () => <div>New Event</div>;
 
 class App extends Component<{}, { selectedView: string }> {
 	render() {
 		return (
 			<div className="container">
-				<NavBar />
-				<Landing />
+				<BrowserRouter>
+					<div>
+						<NavBar />
+						<Route path="/" component={Landing} />
+						<Route exact path="/events" component={Dashboard} />
+						<Route exact path="/events/new" component={CreateEvent} />
+					</div>
+				</BrowserRouter>
 			</div>
 		);
 	}
