251d88ebe35192d48c66f8e7949b69b1a210c56b
Alison Reed <alison.michelle.reed@gmail.com>
2019-01-22 17:10:08 -0500

    Display Event information when successfully retrieved from database

--- client/src/components/events/Event.tsx
+++ client/src/components/events/Event.tsx
@@ -4,20 +4,28 @@ import axios from "axios";
 import { RouteComponentProps } from "react-router-dom";
 
 interface IMatchParams {
-    linkID: string
+  linkID: string
 }
 
 class Event extends Component<{} & RouteComponentProps<IMatchParams>> {
+	state = {
+		createdBy: "",
+		title: ""
+	};
+
 	componentDidMount() {
 		const {linkID} = this.props.match.params;
 		axios.get('/event/' + linkID)
-		.then((response) => console.log('link response', response))
-		.catch((response) => console.log('error', response));
+		.then(({data: {title, createdBy}}) => {
+			this.setState({title, createdBy});
+		})
+		.catch((error) => console.log('error', error));
 	}
 
 	render() {
 		return <div>
-			<h1>Single Event Page</h1>
+			<h1>{this.state.title}</h1>
+			<h5>Event Created By: {this.state.createdBy}</h5>
 		</div>
 	}
 }
