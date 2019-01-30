836e0d74ddff4d6f77ff728c75b53e351b0232be
Alison Reed <alison.michelle.reed@gmail.com>
2019-01-29 19:11:05 -0500

    Move calendar to inside of options creator

--- client/src/components/events/OptionsCreator.tsx
+++ client/src/components/events/OptionsCreator.tsx
@@ -1,7 +1,15 @@
-import * as React from 'react';
-const {Component} = React;
+import * as React from "react";
+const { Component } = React;
 
-import Calendar from '../Calendar';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+import CustomEventOptions from './CustomEventOptions';
+import DayView from "../calendarViews/DayView";
+import MonthView from "../calendarViews/MonthView";
+import WeekView from "../calendarViews/WeekView";
+import YearView from "../calendarViews/YearView";
 
 interface IProps {
 	handleOptionsChange: (optionIndex: number, event: any) => void;
@@ -10,14 +18,54 @@ interface IProps {
 }
 
 class OptionsCreator extends Component<IProps> {
-	render() {
-		const {handleOptionsChange, options, removeOption} = this.props;
-		return <div>
-			Options
-			<Calendar options={options} handleOptionsChange={handleOptionsChange} removeOption={removeOption}/>
-		</div>
+	state = {
+    value: 0,
+  };
+
+  getView(date: any) {
+  	const {options, handleOptionsChange, removeOption} = this.props;
+		switch (this.state.value) {
+			case 4: 
+				return <CustomEventOptions options={options} handleOptionsChange={handleOptionsChange} removeOption={removeOption}/>
+			case 3:
+				return <DayView date={date} />;
+			case 2:
+				return <WeekView date={date} />;
+			case 1:
+				return <MonthView date={date} />;
+			case 0:
+				return <YearView date={date} />;
+			default:
+				return <div />;
+		}
 	}
 
+	handleChange(event: any, value: string) {
+		this.setState({value});
+	}
+
+	render() {
+		const { value } = this.state;
+
+		const date = new Date();
+		const calendarView = this.getView(date);
+
+		return (
+			<div>
+			<h5>Options</h5>
+			  <AppBar position="static">
+	        <Tabs value={value} onChange={(event, value) => this.handleChange(event, value)}>
+	          <Tab label="Year View" />
+	          <Tab label="Month View" />
+	          <Tab label="Week View" />
+	          <Tab label="Day View" />
+	          <Tab label="Custom Options" />
+	        </Tabs>
+        </AppBar>
+					{calendarView}
+        </div>
+		);
+	}
 }
 
 export default OptionsCreator;
