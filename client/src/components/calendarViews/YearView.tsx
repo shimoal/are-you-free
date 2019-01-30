3e1dc03c6b24d2d82d98a01cc099816a787cbf08
Alison Reed <alison.michelle.reed@gmail.com>
2019-01-30 18:02:32 -0500

    Create previous and next arrows for year view

--- client/src/components/calendarViews/YearView.tsx
+++ client/src/components/calendarViews/YearView.tsx
@@ -3,6 +3,9 @@ const { Component } = React;
 import "../../styles/YearView.css";
 
 import { MONTH_NAMES } from "../../helpers/constants";
+
+import Button from "@material-ui/core/Button";
+import Icon from "@material-ui/core/Icon";
 import TimeUnit from "./TimeUnit";
 
 import ICalendarProps from "../../interfaces/ICalendarViewProps";
@@ -11,20 +14,51 @@ interface IProps {
 	options: Array<string>;
 }
 
+interface IState {
+	displayYear: number;
+}
+
 export default class YearView extends Component<ICalendarProps & IProps> {
+	state = {
+		displayYear: this.props.date.getFullYear()
+	};
+
 	render() {
-		const { selectOption, date, options } = this.props;
+		const { selectOption, options } = this.props;
+		const { displayYear } = this.state;
 
 		return (
 			<div className="calendar">
-				<h2>{date.getFullYear()}</h2>
+				<h2>
+					<Button
+						onClick={() =>
+							this.setState((prevState: IState) => ({
+								displayYear: prevState.displayYear - 1
+							}))
+						}
+					>
+						<Icon>arrow_left</Icon>
+						Previous
+					</Button>
+					{this.state.displayYear}
+					<Button
+						onClick={() =>
+							this.setState((prevState: IState) => ({
+								displayYear: prevState.displayYear + 1
+							}))
+						}
+					>
+						Next
+						<Icon>arrow_right</Icon>
+					</Button>
+				</h2>
 				<div id="yearView">
 					{MONTH_NAMES.map((name, i) => {
 						return (
 							<TimeUnit
 								selectOption={selectOption}
 								options={options}
-								label={name}
+								label={`${name} ${displayYear}`}
 								key={i}
 								height={100}
 								width={100}
