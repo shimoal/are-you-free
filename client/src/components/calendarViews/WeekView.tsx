f8ab1e6e938bea260595b9fafd06681b32e525d1
Alison Reed <alison.michelle.reed@gmail.com>
2019-01-31 11:38:52 -0500

    Include all date data on week view

--- client/src/components/calendarViews/WeekView.tsx
+++ client/src/components/calendarViews/WeekView.tsx
@@ -2,8 +2,8 @@ import * as React from "react";
 const { Component } = React;
 import "../../styles/WeekView.css";
 
-import { DAY_NAMES } from "../../helpers/constants";
-import { getWeekDates } from "../../helpers/utils";
+import { DAY_NAMES, MONTH_NAMES } from "../../helpers/constants";
+import { getAllWeekDates, getWeekDates } from "../../helpers/utils";
 import TimeUnit from "./TimeUnit";
 
 import ICalendarProps from "../../interfaces/ICalendarViewProps";
@@ -15,17 +15,23 @@ interface IProps {
 export default class WeekView extends Component<ICalendarProps & IProps> {
 	render() {
 		const { selectOption, date, options } = this.props;
+		const weekDates = getAllWeekDates(date);
 
 		return (
 			<div className="calendar">
 				<h2>{getWeekDates(date)}</h2>
 				<div id="weekView">
 					{DAY_NAMES.map((name, i) => {
+						const dateInfo = weekDates[i];
+
+						const label = `${name} ${
+							MONTH_NAMES[dateInfo.getMonth()]
+						} ${dateInfo.getDate()} ${dateInfo.getFullYear()}`;
 						return (
 							<TimeUnit
 								selectOption={selectOption}
 								options={options}
-								label={name}
+								label={label}
 								key={i}
 								height={300}
 								width={60}
